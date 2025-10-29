b6ab9d42f0483456a62f9bc897397c60d6c57459
choijaewoo <cchoi00332244@gmail.com>
2025-10-29 23:28:27 +0900

    feat(Calc): 구매한 Lotto List 를 당첨번호와 비교하여 결과를 계산

--- src/main/java/lotto/service/LottoScoreCalc.java
+++ src/main/java/lotto/service/LottoScoreCalc.java
@@ -1,5 +1,30 @@
 package lotto.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.Lotto;
+import lotto.domain.WinningLotto;
+
 public class LottoScoreCalc {
 
+    private final WinningLotto winningLotto;
+    public LottoScoreCalc(WinningLotto winningLotto) {
+        this.winningLotto = winningLotto;
+    }
+
+    public Rank calculate(Lotto userLotto) {
+        int matchCount = winningLotto.countMatch(userLotto);
+        boolean hasBonus = winningLotto.hasBonus(userLotto);
+        return Rank.getRank(matchCount, hasBonus);
+    }
+
+    public Map<Rank, Long> getResult(List<Lotto> lottos) {
+        Map<Rank, Long> result = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            Rank rank = calculate(lotto);
+            result.put(rank, result.getOrDefault(rank, 0) + 1);
+        }
+        return result;
+    }
 }
