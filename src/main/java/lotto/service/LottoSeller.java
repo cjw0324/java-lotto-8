49214130bc2f9640c3826c779bfdf02d84baee0e
choijaewoo <cchoi00332244@gmail.com>
2025-10-29 23:09:47 +0900

    feat(LottoSeller): 판매자 로직에 금액 검증 추가

--- src/main/java/lotto/service/LottoSeller.java
+++ src/main/java/lotto/service/LottoSeller.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import static lotto.exception.Exception.NOT_VALID_MONEY;
+
 import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
@@ -12,11 +14,18 @@ public class LottoSeller {
         this.generator = generator;
     }
 
-    public List<Lotto> selling(int totalMoney) {
+    public List<Lotto> selling(int money) {
+        validatePurchaseAmount(money);
         List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < totalMoney / LOTTO_PRICE; i++) {
+        for (int i = 0; i < money / LOTTO_PRICE; i++) {
             lottos.add(generator.generate());
         }
         return lottos;
+    }
+
+    private void validatePurchaseAmount(int money) {
+        if (money < LOTTO_PRICE || money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(NOT_VALID_MONEY.print());
+        }
     }
 }
