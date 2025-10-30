4983d53764cdf9b27a0ab6a1261fb2d185f46deb
choijaewoo <cchoi00332244@gmail.com>
2025-10-30 12:53:25 +0900

    feat(factory): user 생성을 담당하는 UserFactory 구현

--- src/main/java/lotto/factory/UserFactory.java
+++ src/main/java/lotto/factory/UserFactory.java
@@ -1,4 +1,36 @@
 package lotto.factory;
 
+import lotto.domain.User;
+import lotto.io.Input;
+import lotto.io.Output;
+import lotto.service.LottoSeller;
+
 public class UserFactory {
+    private final Input input;
+    private final Output output;
+    private final LottoSeller lottoSeller;
+
+    public UserFactory(Input input, Output output, LottoSeller lottoSeller) {
+        this.input = input;
+        this.output = output;
+        this.lottoSeller = lottoSeller;
+    }
+
+    public User createUser() {
+        User user = new User();
+        output.printAskPrice();
+
+        while (true) {
+            try {
+                int price = input.readPrice();
+                user.setPurchaseAmount(price);
+                user.buy(lottoSeller);
+                output.printBuyingTickets(user.getLottoList());
+                return user;
+            } catch (IllegalArgumentException e) {
+                output.print(e.getMessage());
+                output.printRetry();
+            }
+        }
+    }
 }
