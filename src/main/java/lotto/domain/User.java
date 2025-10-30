21c2f739c8027e2d71960f2b66633414c57e8428
choijaewoo <cchoi00332244@gmail.com>
2025-10-30 12:51:43 +0900

    refactor(factory): user, winningLotto 에 Factory 패턴을 적용하여 객체 생성의 책임을 Controller 에서 Factory로 이전

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -2,8 +2,10 @@ package lotto.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import lotto.Lotto;
 import lotto.service.LottoSeller;
+import lotto.service.Rank;
 
 public class User {
     private final List<Lotto> lottoList;
@@ -51,5 +53,13 @@ public class User {
 
     public double getYield() {
         return yield;
+    }
+
+    public void calculateEarnings(Map<Rank, Long> result) {
+        this.totalEarnings = result.entrySet().stream()
+                .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue())
+                .sum();
+
+        this.yield = Math.round(((double) totalEarnings / purchaseAmount) * 1000) / 10.0;
     }
 }
